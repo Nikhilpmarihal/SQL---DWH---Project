f9933c47c526a01e471c60255a4f374659d38af0
Nikhil P Marihal <nikhilpmarihal9@gmail.com>
2025-04-16 00:16:34 +0530

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -8,11 +8,8 @@ USE DataWareHouse;
 
 --GO - SEPARATES BATCHES WHEN WORKING WITH MULTIPLE SQL STATEMENTS
 CREATE SCHEMA bronze;
-GO
 CREATE SCHEMA silver;
-GO
 CREATE SCHEMA gold;
-GO
 
 
 
@@ -102,3 +99,68 @@ SELECT *
 FROM bronze.crm_cust_info
 
 SELECT COUNT(*) FROM bronze.crm_cust_info
+
+BULK INSERT bronze.crm_prd_info
+FROM 'C:\Users\Asus\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+SELECT *
+FROM bronze.crm_prd_info
+
+SELECT COUNT(*) FROM bronze.crm_prd_info
+
+BULK INSERT bronze.crm_sales_details
+FROM 'C:\Users\Asus\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+SELECT *
+FROM bronze.crm_sales_details
+
+SELECT COUNT(*) FROM bronze.crm_sales_details
+
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\Users\Asus\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp/CUST_AZ12.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+SELECT *
+FROM bronze.erp_cust_az12
+
+SELECT COUNT(*) FROM bronze.erp_cust_az12
+
+BULK INSERT bronze.erp_loc_a101
+FROM 'C:\Users\Asus\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp/LOC_A101.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+SELECT *
+FROM bronze.erp_loc_a101
+
+SELECT COUNT(*) FROM bronze.erp_loc_a101
+
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\Users\Asus\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp/PX_CAT_G1V2.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+SELECT *
+FROM bronze.erp_px_cat_g1v2 
+
+SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2
